ff155c32e98c184defd2345e6a5dc9714a2e5a09
Gaston <Hyouka21@github.com>
2020-09-18 00:47:26 -0300

    Primera version de tp7 modificando metodo main

--- src/TestDirectorio/TpdeLaboratorio7.java
+++ src/TestDirectorio/TpdeLaboratorio7.java
@@ -6,7 +6,7 @@ public class TpdeLaboratorio7 {
 
 
     public static void main(String[] args) {
-    Cliente c1 = new Cliente("Luis","Rosales","Mendoza","B17mza2casa4",39092451);
+    Cliente c1 = new Cliente("Luis","Rosales","Mendoza","B17mza2casa4",44445);
     Cliente c2 = new Cliente("Emanuel","Salguero","Cordoba","B17mza2casa4",11111);
     Cliente c3 = new Cliente("Rebeca","Sosa","San Luis","B17mza2casa4",22222);
     Cliente c4 = new Cliente("Maria","Gomez","San Luis","B17mza2casa4",33333);
